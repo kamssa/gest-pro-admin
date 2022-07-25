4aba1a66ea191d358c54041b8ea26bb03862766d
Traore <kamssa0@gmail.com>
2022-07-25 16:35:58 +0000

    update memory

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
    production: false,
-   apiUrl: 'http://localhost:8080',
-  // apiUrl: 'https://gest-pro-backend.herokuapp.com/'
+   //apiUrl: 'http://localhost:8080',
+   apiUrl: 'https://gest-pro-backend.herokuapp.com/'
 };
 
 /*
