9c998a90e69de4cf835dcb420a990f046203fdff
Traore <kamssa0@gmail.com>
2022-08-19 12:52:26 +0000

    cumul

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   apiUrl: 'http://localhost:8080',
-   //apiUrl: 'https://gest-pro-backend.herokuapp.com/'
+   //apiUrl: 'http://localhost:8080',
+   apiUrl: 'https://gest-pro-backend.herokuapp.com/'
 };
